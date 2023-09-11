9cf03c25e475fd9b11d020ad9ac130c97b4825b0
Tony Liu <tony.liu@yahoo.com>
2023-09-11 15:05:51 -0500

    add/update anaconda release

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
-"%PYTHON%" setup.py install
-if errorlevel 1 exit 1
+#!/bin/bash
+
+export PREFIX=$PREFIX
+export PYTHON=$PREFIX/bin/python
+export PATH=$PATH:~/.local/bin
+
+echo "PREFIX=${PREFIX}"
+echo "PYTHON=${PYTHON}"
+
+poetry export --without-hashes --format=requirements.txt > ./requirements-poetry.txt
+
+# Use pip to install the dependencies to a specific directory
+pip install -r requirements-poetry.txt --target $PREFIX/lib/python3.10/site-packages --upgrade
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="snowflake-ai",
-    version="0.5.3",
+    version="0.5.4",
     author="Illuminairy AI",
     author_email="tony.liu@yahoo.com",
     description="A Snowflake centic Enterprise AI/ML framework with tight integration of popular data science libraries",
@@ -13,12 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/illuminairy-ai/snowflake-ai",
     packages=setuptools.find_packages(),
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: Apache Software License", 
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Software Development :: Libraries :: Python Modules"
-    ),
+    ],
 )
